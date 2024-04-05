51315bbe28be28410f19f991f5878a32c81a6eb2
m-mueller678 <marcus.mueller.678@gmail.com>
2024-04-05 21:34:28 +0200

    support non-finites

--- src/format_impl.rs
+++ src/format_impl.rs
@@ -9,11 +9,11 @@ pub(crate) trait FormatImpl: Copy {
 
 impl FormatImpl for f64 {
     fn format_impl(mut self, config: &Config, out: &mut [u8; BUFFER_SIZE]) -> usize {
-        assert!(self.is_finite());
         #[allow(clippy::assertions_on_constants)]
         const _: () = {
             assert!(BUFFER_SIZE >= 30);
         };
+        assert!(config.significant_digits <= 15);
         let is_negative = self.is_sign_negative();
         let out = if is_negative {
             out[0] = b'-';
@@ -22,55 +22,59 @@ impl FormatImpl for f64 {
         } else {
             &mut out[..]
         };
-        assert!(config.significant_digits <= 15);
-        self *= 10f64.powi(config.shift as i32);
-        let log1000 = if self==0.0{0}else{(self.log10() / 3.0 - 1e-4).floor() as i32};
-        let normalized = self / 1000f64.powi(log1000);
         let mut writer = WriteBuffer {
             buffer: out,
             written: 0,
         };
         let std_precision = config.significant_digits - 1;
-        core::fmt::write(
-            &mut writer,
-            format_args!("{:.*}", std_precision, normalized),
-        )
-        .unwrap();
-        dbg!(String::from_utf8_lossy(&writer.buffer));
-        let decimal_pos = writer.written - std_precision - 1;
-        if writer.buffer[decimal_pos] == b'.' {
-            if decimal_pos > 3 {
-                debug_assert!(decimal_pos == 4);
-                writer.buffer[1..][..4].rotate_right(1);
-            }
-            'format_number: {
-                if decimal_pos >= config.significant_digits {
-                    writer.written = decimal_pos;
-                    break 'format_number;
-                }
-                let decimal_places = config.significant_digits - decimal_pos;
-                for i in (0..decimal_places).rev() {
-                    writer.buffer[decimal_pos + 1 + i + i / 3] = writer.buffer[decimal_pos + 1 + i];
-                    if i % 3 == 2 {
-                        writer.buffer[decimal_pos + 1 + i + i / 3 + 1] = b'_';
-                    }
-                }
-                let last_decimal = decimal_places - 1;
-                writer.written = decimal_pos + 1 + last_decimal + last_decimal / 3 + 1;
-                // at most 25
+        if self.is_finite() {
+            self *= 10f64.powi(config.shift as i32);
+            let log1000 = if self == 0.0 {
+                0
+            } else {
+                (self.log10() / 3.0 - 1e-4).floor() as i32
             };
+            self *= 1000f64.powi(-log1000);
+            core::fmt::write(&mut writer, format_args!("{:.*}", std_precision, self)).unwrap();
+            //dbg!(String::from_utf8_lossy(writer.buffer));
+            let decimal_pos = writer.written - std_precision - 1;
+            if writer.buffer[decimal_pos] == b'.' {
+                if decimal_pos > 3 {
+                    debug_assert!(decimal_pos == 4);
+                    writer.buffer[1..][..4].rotate_right(1);
+                }
+                'format_number: {
+                    if decimal_pos >= config.significant_digits {
+                        writer.written = decimal_pos;
+                        break 'format_number;
+                    }
+                    let decimal_places = config.significant_digits - decimal_pos;
+                    for i in (0..decimal_places).rev() {
+                        writer.buffer[decimal_pos + 1 + i + i / 3] =
+                            writer.buffer[decimal_pos + 1 + i];
+                        if i % 3 == 2 {
+                            writer.buffer[decimal_pos + 1 + i + i / 3 + 1] = b'_';
+                        }
+                    }
+                    let last_decimal = decimal_places - 1;
+                    writer.written = decimal_pos + 1 + last_decimal + last_decimal / 3 + 1;
+                    // at most 25
+                };
+            } else {
+                debug_assert!(!writer.buffer[..writer.written].iter().any(|x| *x == b'.'));
+            }
+            if !(-10..=10).contains(&log1000) {
+                writer.push_byte(b'e');
+                write!(&mut writer, "{log1000}").unwrap();
+            } else if log1000 == -2 {
+                writer.write_str("µ").unwrap();
+            } else if log1000 != 0 {
+                writer.push_byte(b"qryzafpnum kMGTPEZYRQ"[(log1000 + 10) as usize]);
+            }
+            debug_assert!(writer.written <= 29);
         } else {
-            debug_assert!(!writer.buffer[..writer.written].iter().any(|x| *x == b'.'));
+            core::fmt::write(&mut writer, format_args!("{:.*}", std_precision, self)).unwrap();
         }
-        if !(-10..=10).contains(&log1000) {
-            writer.push_byte(b'e');
-            write!(&mut writer, "{log1000}").unwrap();
-        } else if log1000 == -2 {
-            writer.write_str("µ").unwrap();
-        } else if log1000 != 0 {
-            writer.push_byte(b"qryzafpnum kMGTPEZYRQ"[(log1000 + 10) as usize]);
-        }
-        debug_assert!(writer.written <= 29);
         writer.written + is_negative as usize
     }
 }
